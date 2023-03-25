c8703972040a8a986a3acd89fa3f26692e5e0606
NicoAL95 <nicoabellaia@gmail.com>
2023-03-26 01:02:26 +0700

    Finish basic CRD features

--- app.js
+++ app.js
@@ -1,12 +1,84 @@
+const { demandOption } = require("yargs");
+const yargs = require("yargs");
+const { simpanContact, listContact, detailContact, deleteContact } = require("./contacts");
+
 // const contacts = require('./contacts')
-const { tulisPertanyaan, simpanContact } = require('./contacts')
+// const { tulisPertanyaan, simpanContact } = require('./contacts')
 
-const main = async () => {
-    const nama = await tulisPertanyaan('Masukkan nama anda : ')
-    const email = await tulisPertanyaan('Masukkan email anda: ')
-    const noHp = await tulisPertanyaan('Masukkan nomor hp anda: ')
+// const main = async () => {
+//     const nama = await tulisPertanyaan('Masukkan nama anda : ')
+//     const email = await tulisPertanyaan('Masukkan email anda: ')
+//     const noHp = await tulisPertanyaan('Masukkan nomor hp anda: ')
 
-    simpanContact(nama, email, noHp)
-}
+//     simpanContact(nama, email, noHp)
+// }
 
-main()
+// main()
+
+// console.log(yargs.argv);
+
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string'
+        },
+        email: {
+            describe: 'Email',
+            demandOption: false,
+            type: 'string'
+        },
+        noHP: {
+            describe: 'Nomor handphone', demandOption: true,
+            type: 'string'
+        }
+    }, handler(argv) {
+        simpanContact(argv.nama, argv.email, argv.noHP)
+    }
+}).demandCommand();
+
+// Menampilkan daftar semua nama & no hp contact
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua nama & no HP contact',
+    handler() {
+        listContact();
+    }
+})
+
+// Menampilkan detail contact
+yargs.command({
+    command: 'detail',
+    describe: 'Menampilkan detail dari sebuah contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        detailContact(argv.nama)
+    }
+})
+
+// Mengahpus contact berdasarkan nama
+yargs.command({
+    command: 'delete',
+    describe: 'Menghapus sebuah contact berdasarkan nama',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        deleteContact(argv.nama)
+    }
+})
+
+yargs.parse();
--- contacts.js
+++ contacts.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
-
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const chalk = require('chalk')
+const validator = require('validator')
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
 // Membuat folder data jika tidak ada
 const dirPath = './data'
@@ -18,12 +19,22 @@ if(!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
-const tulisPertanyaan = (pertanyaan) => {
-    return new Promise((resolve, reject) => {
-        rl.question(pertanyaan, (setPertanyaan) => {
-            resolve(setPertanyaan)
-        })
-    })
+// const tulisPertanyaan = (pertanyaan) => {
+//     return new Promise((resolve, reject) => {
+//         rl.question(pertanyaan, (setPertanyaan) => {
+//             resolve(setPertanyaan)
+//         })
+//     })
+// }
+
+const loadContact = () => {
+    // Baca FIle
+    const file = fs.readFileSync('data/contacts.json', 'utf-8');
+    
+    // Convert ke JSON
+    const contacts = JSON.parse(file)
+
+    return contacts
 }
 
 const simpanContact = (nama, email, noHp ) => {
@@ -32,18 +43,85 @@ const simpanContact = (nama, email, noHp ) => {
         email,
         noHp
     }
-    // Baca FIle
-    const file = fs.readFileSync('data/contacts.json', 'utf-8');
-    // Convert ke JSON
-    const contacts = JSON.parse(file)
+
+    // Membaca file
+    const contacts = loadContact()
+
+    // Cek Duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama)
+    if(duplikat) {
+        console.log(chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!'));
+        return false
+    }
+
+    // Cek email
+    if(email){
+        if(!validator.isEmail(email)){
+            console.log(chalk.red.inverse.bold('Email tidak valid!'));
+            return false
+        }
+    }
+
+    // Cek nomor hp
+    if(!validator.isMobilePhone(noHp, 'id-ID')){
+        console.log(chalk.red.inverse.bold('Nomor HP tidak valid!'));
+        return false
+    }
+
 
     // Memasukkan data ke dalam JSON
     contacts.push(contact)
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null, 2));
 
-    console.log('Terimakasih sudah input data!');
-    rl.close();
+    console.log(chalk.green.inverse('Terimakasih sudah input data!'));
+    // rl.close();
 }
 
-module.exports = { tulisPertanyaan, simpanContact }
+// Menampilkan list contact
+const listContact = () => {
+    const contacts = loadContact()
+
+    console.log(chalk.green.inverse('Daftar Contact:'));
+
+    contacts.forEach((contact, i) => {
+        console.log(`${i+1}. ${contact.nama} - ${contact.noHp}`);
+    })
+}
+
+// Menampilkan detail contact dari nama
+const detailContact = (nama) => {
+    const contacts = loadContact()
+
+    const findeDetail = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase())
+
+    if(!findeDetail){
+        console.log(chalk.red.inverse(`Maaf ${nama} Tidak ditemukan!`));
+        return false
+    } 
+    
+    console.log(chalk.green.inverse(`Selamat, data ${nama} berhasil ditemukan!`));
+    console.log(`Nama: ${findeDetail.nama}`)
+    if (findeDetail.email) {
+        console.log(`Email: ${findeDetail.email}`)
+    }
+    console.log(`noHp: ${findeDetail.noHp}`)
+}
+
+// Menghapus contact dari nama
+const deleteContact = (nama) => {
+    const contacts = loadContact()
+    const newContacts = contacts.filter((contact) => contact.nama.toLowerCase() !== nama.toLowerCase())
+
+    if(contacts.length === newContacts.length){
+        console.log(chalk.red.inverse(`Maaf ${nama} Tidak ditemukan!`));
+        return false
+    }
+
+    fs.writeFileSync('data/contacts.json', JSON.stringify(newContacts, null, 2));
+
+    console.log(chalk.green.inverse(`Data ${nama} Berhasil Dihapus!`));
+}
+
+
+module.exports = { simpanContact, listContact, detailContact, deleteContact }
